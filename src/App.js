ed63ccb8c7fc75f60dbf90eba9e6015eca109cbd
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-11-15 19:49:53 +0500

    asdadadsa

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Section2 from './components/Section2';
 import Section6 from './components/Section6';
 import Section11 from './components/Section11';
 import Footer from './components/Footer';
+import Section3 from './components/Section3';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Section2/>
+    <Section3/>
     <Section11/>
     <Section6/>
     <Footer/>
--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -12,27 +12,27 @@ const Section6 = () => {
 
         <div className="space-y-6 max-w-md">
           <div className="text-center md:text-left">
-            <p className="text-lg text-gray-400">
+            <p className="text-lg text-start lg:text-center text-gray-400">
               Agar siz ham o'zingizga ishonch va baxt tuyg'usini his qilishni istasangiz
             </p>
-            <h1 className="text-4xl font-serif font-bold mt-4">
+            <h1 className="text-4xl font-serif text-start lg:text-center lg:font-bold mt-4">
               Sizni Webinarimda kutaman!
             </h1>
           </div>
 
           <div className="space-y-4">
-            <div className="flex w-[380px] items-center gap-3 bg-gray-800 rounded-xl p-2 shadow-md">
+            <div style={{background: 'linear-gradient(90deg, rgba(172, 172, 172, 0.2) 0%, rgba(70, 70, 70, 0.2) 50%, rgba(172, 172, 172, 0.2) 100%)'}} className="flex max-w-[100%] items-center gap-3 bg-gray-800 rounded-xl px-2 py-3 shadow-md">
               <span className="text-gray-400">Mavzu:</span>
               <span className="text-gray-200 font-medium">Haqiqiy baxtga yetish uchun 5 qadam</span>
             </div>
-            <div className="flex w-[300px] items-center gap-3 bg-gray-800 rounded-xl p-2 shadow-md ">
+            <div style={{background: 'linear-gradient(90deg, rgba(172, 172, 172, 0.2) 0%, rgba(70, 70, 70, 0.2) 50%, rgba(172, 172, 172, 0.2) 100%)'}} className="flex max-w-[80%] items-center gap-3 bg-gray-800 rounded-xl px-2 py-3 shadow-md ">
               <span className="text-gray-400">Spiker:</span>
               <span className="text-gray-200 font-medium">Nadia Abdullaxodjayeva</span>
             </div>
           </div>
 
           <div >
-          <button className="py-3 px-12 rounded-lg font-medium text-white bg-gradient-to-r from-[#660116] via-[#C73659] to-[#A91D3A] hover:from-[#A91D3A] hover:to-[#660116] transition duration-300">
+          <button className="py-4  px-12 w-full lg:max-w-[90%] lg:mt-10 mt-5 rounded-lg font-medium text-white bg-gradient-to-r from-[#660116] via-[#C73659] to-[#A91D3A] hover:from-[#A91D3A] hover:to-[#660116] transition duration-300">
             Ro’yhatdan o’tish
         </button>
           </div>
