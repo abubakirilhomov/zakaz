2147395d81a4360cc266c146055425751522bac3
Asadbek-2211 <asadbeksunnatullayev7766@gmail.com>
2024-11-15 19:26:46 +0500

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { FaInstagram, FaYoutube, FaTelegramPlane } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-gray-500 py-8 px-4">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="text-sm md:text-left text-center mb-4 md:mb-0">
-          <p className="font-semibold">
+    <footer className="bg-black text-gray-400 py-8 px-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start p-2">
+        <div className="text-sm text-start p-1 md:text-left mb-4 md:mb-0 space-y-1">
+          <p className="font-semibold text-white">
             "NADIA'S SCHOOL" MAS'ULIYATI CHEKLANGAN JAMIYAT
           </p>
           <p>ИНН: 311141396</p>
@@ -15,17 +15,17 @@ const Footer = () => {
           <p>Банк: Тошкент Ш., "ИПАК ЙУЛИ" АИТ БАНКИНИНГ САФБОН ФИЛИАЛИ</p>
         </div>
 
-        <div className="text-white">
-          <div className="text-sm   space-y-2 mb-4 md:mb-0">
-            <a href="#" className="block hover:text-gray-400">
+        <div className="flex flex-col items-start md:items-start text-white space-y-4">
+          <div className="text-sm space-y-2">
+            <a href="#" className="block hover:text-gray-300">
               Публичная оферта
             </a>
-            <a href="#" className="block hover:text-gray-400">
+            <a href="#" className="block hover:text-gray-300">
               Политика конфиденциальности
             </a>
           </div>
 
-          <div className="flex space-x-4 text-pink-500 mt-5">
+          <div className="flex space-x-4 text-pink-500">
             <a href="#" className="hover:text-pink-400">
               <FaInstagram size={20} />
             </a>
@@ -38,8 +38,8 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <hr className="border-gray-600" />
-      <div className="text-center text-gray-500 mt-4 text-xs">
+      <hr className="border-gray-600 my-4" />
+      <div className="text-center text-gray-500 text-xs">
         © 2024 Nadia’s school. Все права защищены
       </div>
     </footer>
